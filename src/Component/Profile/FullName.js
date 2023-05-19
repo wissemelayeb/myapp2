f22eb0c53c9feaca659fec12f58c15271b21ec72
Wissemelayeb <127353254+wissemelayeb@users.noreply.github.com>
2023-05-19 20:01:18 +0100

    Update FullName.js

--- src/Component/Profile/FullName.js
+++ src/Component/Profile/FullName.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+// FullName component
 function FullName() {
   return (
+    // Container for the full name
     <div className="full-name-container">
+      {/* Heading for the full name */}
       <h2 className="full-name">Wissem Elayeb</h2>
     </div>
   );
