7e44107efa80f803ae6e10579b5be112ab1dc682
Wissemelayeb <127353254+wissemelayeb@users.noreply.github.com>
2023-05-19 20:01:58 +0100

    Update PhotoProfile.js

--- src/Component/Profile/PhotoProfile.js
+++ src/Component/Profile/PhotoProfile.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import MESSI from '../../Image/MESSI.jpg'
+import MESSI from '../../Image/MESSI.jpg';
+
+// ProfilePhoto component
 function ProfilePhoto() {
   return (
+    // Container for the profile photo
     <div className="profile-photo-container">
+      {/* Profile photo */}
       <img className="profile-photo" src={MESSI} alt="MyPhoto" />
     </div>
   );
